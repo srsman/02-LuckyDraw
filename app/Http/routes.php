8b2c7cfa960ade86e58c5a9fd68aaccef7480cfa
Loyalsoldier <loyalsoldier@qq.com>
2016-03-17 12:30:39 +0800

    新增：管理后台路由

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/admin', function () {
+    return view('admin');
+});
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
